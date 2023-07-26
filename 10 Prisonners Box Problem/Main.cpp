52fd527125155eee6d5745364b9e78b127538a63
Ryan Pek <ryanpekyize2001@gmail.com>
2023-07-26 14:24:38 +0800

    main

---
+++ 10 Prisonners Box Problem/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int generate_testvalues() {
+	
+}
+
+long long factorialfn() {
+
+}
+
+int main() {
+	std::cout << "hello world";
+
+	return 0;
+}
---
+++ Python Attempt/Python_Attempt.py
@@ -0,0 +1,19 @@
+import json
+
+
+def generatesampleboxdata():
+    superlist = []
+    
+
+    return superlist
+
+def main():
+    print('Start')
+    generatesampleboxdata()
+    print('End')
+    return
+
+
+if __name__ == "__main__":
+    main()
+
